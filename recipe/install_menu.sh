7c2d08e7feb43ee8347c8fb486f8ee114c20f1a7
Jaime RGP <jaimergp@users.noreply.github.com>
2021-10-07 13:49:06 +0200

    adjust icons per platform

--- recipe/install_menu.sh
+++ recipe/install_menu.sh
@@ -3,9 +3,10 @@
 set -euxo pipefail
 
 mkdir -p "${PREFIX}/Menu"
-cp "${SRC_DIR}/resources/icon.ico" "${PREFIX}/Menu/napari.ico"
 if [[ $target_platform == linux* ]]; then
     cp "${RECIPE_DIR}/menu-linux.json" "${PREFIX}/Menu/napari.json"
+    cp "${SRC_DIR}/napari/resources/logo.png" "${PREFIX}/Menu/napari.png"
 else
     cp "${RECIPE_DIR}/menu-osx.json" "${PREFIX}/Menu/napari.json"
+    cp "${SRC_DIR}/resources/icon.icns" "${PREFIX}/Menu/napari.icns"
 fi
