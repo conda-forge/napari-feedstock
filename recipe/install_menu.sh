e38101f45692afc6d5716acd52a31e9a87ed12f1
Jaime RGP <jaimergp@users.noreply.github.com>
2021-10-20 14:14:02 +0200

    update paths

--- recipe/install_menu.sh
+++ recipe/install_menu.sh
@@ -8,5 +8,5 @@ if [[ $target_platform == linux* ]]; then
     cp "${SRC_DIR}/napari/resources/logo.png" "${PREFIX}/Menu/napari.png"
 else
     cp "${RECIPE_DIR}/menu-osx.json" "${PREFIX}/Menu/napari.json"
-    cp "${SRC_DIR}/resources/icon.icns" "${PREFIX}/Menu/napari.icns"
+    cp "${SRC_DIR}/napari/resources/icon.icns" "${PREFIX}/Menu/napari.icns"
 fi
