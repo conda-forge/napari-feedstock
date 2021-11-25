2f8950c22e7e3452189515f51bb8161913787952
Jaime RGP <jaimergp@users.noreply.github.com>
2021-11-25 12:45:37 +0100

    add version to menu

--- recipe/install_menu.bat
+++ recipe/install_menu.bat
@@ -1,3 +1,3 @@
 if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
-copy "%RECIPE_DIR%\menu.json" "%PREFIX%\Menu\napari.json" || exit 1
+sed "s/__PKG_VERSION__/%PKG_VERSION%/" "%RECIPE_DIR%\menu.json" > "%PREFIX%\Menu\napari.json" || exit 1
 copy "%SRC_DIR%\napari\resources\icon.ico" "%PREFIX%\Menu\napari.ico" || exit 1
