07d3ab51b6a78b57679dced8348d99af7fe4b3f9
Jaime RGP <jaimergp@users.noreply.github.com>
2021-12-08 16:41:23 +0100

    fix empty menu

--- recipe/install_menu.sh
+++ recipe/install_menu.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 mkdir -p "${PREFIX}/Menu"
-sed -i.bak "s/__PKG_VERSION__/${PKG_VERSION}/" "${RECIPE_DIR}/menu.json" > "${PREFIX}/Menu/napari.json"
+sed "s/__PKG_VERSION__/${PKG_VERSION}/" "${RECIPE_DIR}/menu.json" > "${PREFIX}/Menu/napari.json"
 if [[ $target_platform == linux* ]]; then
     cp "${SRC_DIR}/napari/resources/logo.png" "${PREFIX}/Menu/napari.png"
 else
