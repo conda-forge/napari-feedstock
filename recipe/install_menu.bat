61979a1a1cb4a71f60cc6d5357ef7b466c986905
Jaime RGP <jaimergp@users.noreply.github.com>
2021-12-30 11:11:42 +0100

    make menu noarch

--- recipe/install_menu.bat
+++
@@ -1,3 +0,0 @@
-if not exist "%PREFIX%\Menu" mkdir "%PREFIX%\Menu"
-sed "s/__PKG_VERSION__/%PKG_VERSION%/" "%RECIPE_DIR%\menu.json" > "%PREFIX%\Menu\napari.json" || exit 1
-copy "%SRC_DIR%\napari\resources\icon.ico" "%PREFIX%\Menu\napari.ico" || exit 1
