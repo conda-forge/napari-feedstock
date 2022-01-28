672143659a3e1586512cff3143c3b6b3430a6dcb
Jaime RGP <jaimergp@users.noreply.github.com>
2022-01-28 15:19:53 +0100

    MNT: Re-rendered with conda-build 3.21.4, conda-smithy 3.16.2, and conda-forge-pinning 2022.01.28.10.42.27

--- .scripts/build_steps.sh
+++ .scripts/build_steps.sh
@@ -42,7 +42,7 @@ source run_conda_forge_build_setup
 # "recipe/yum_requirements.txt" file. After updating that file,
 # run "conda smithy rerender" and this line will be updated
 # automatically.
-/usr/bin/sudo -n yum install -y mesa-libGL-devel xorg-x11-server-Xvfb libXrender mesa-dri-drivers
+/usr/bin/sudo -n yum install -y mesa-libGL-devel xorg-x11-server-Xvfb libXrender mesa-dri-drivers dbus-libs libxkbcommon-x11
 
 
 # make the build number clobber
